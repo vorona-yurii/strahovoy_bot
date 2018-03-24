3c72e6826935abb0ca51ccda4dbfb7f37374bb27
yuv <voronayu1331@gmail.com>
2018-03-24 17:39:30 +0200

    5.15

--- function.php
+++ function.php
@@ -162,11 +162,11 @@ function OrderTotal($user_id){
         $order_total = $order_total * 1.5;
     }
 
-    if($order['tarif'] == "Расширенный"){
-        $order_total = $order_total * 1.2;
-    }elseif($order['tarif'] == "Путешествие на авто"){
-        $order_total = $order_total * 1.6;
-    }
+//    if($order['tarif'] == "Расширенный"){
+//        $order_total = $order_total * 1.2;
+//    }elseif($order['tarif'] == "Путешествие на авто"){
+//        $order_total = $order_total * 1.6;
+//    }
 
     return $order_total;
 
