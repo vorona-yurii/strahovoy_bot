d3af4055ec8a3947eec7623da9deefefdf99f81d
yuv <voronayu1331@gmail.com>
2018-04-04 22:35:23 +0300

    12.08

--- setwebhook.php
+++ setwebhook.php
@@ -1,24 +1,10 @@
 <?php
+require_once "vendor/autoload.php";
 require 'config.php';
 
-function regHandler($token, $murl)
-{
-    $url = "https://api.telegram.org/bot" . $token . "/setWebhook";
-    $ch = curl_init();
-    $optArray = array(
-        CURLOPT_URL => $url,
-        CURLOPT_POST => true,
-        CURLOPT_RETURNTRANSFER => true,
-        CURLOPT_POSTFIELDS => array('url' => $murl)
-    );
-    curl_setopt_array($ch, $optArray);
+use Telegram\Bot\Api;
 
-    $result = curl_exec($ch);
-    echo "<pre>";
-    print_r($result);
-    echo "</pre>";
-    curl_close($ch);
-}
+$telegram = new Api(BOT_API_KEY);
+$response = $telegram->setWebhook(['url' => HANDLERURL]);
 
-regHandler(BOT_API_KEY, HANDLERURL);
 ?>
