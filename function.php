900a56d17414dc5e64a1a4dfd616c32831953193
yuv <voronayu1331@gmail.com>
2018-04-21 13:38:40 +0300

    test 15.14

--- function.php
+++ function.php
@@ -289,6 +289,8 @@ function OrderTotal($user_id){
         $coff = $coff + $coff_admin['value']; // обавляем общий коэффициент
     }
 
+    return $coff;
+
     $order_total = $order_total + $order_total * $coff;
     $order_total = $order_total * getApiNBU($valut); //курс валюты умножаем на окончательную сумму
 
